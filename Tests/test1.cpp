e1ea51c20a10b1ed2ff6ccdbe260e1e5b4e4bd77
Hank Gansert <hgansert@breadware.com>
2019-05-22 14:23:04 -0700

    Added minimal funcitonality

--- Tests/test1.cpp
+++ Tests/test1.cpp
@@ -1,6 +1,35 @@
-#include namespace std;
+#include <iostream>
+using namespace std;
 
-main()
+
+
+
+class A
 {
-  
+  public:
+  void func(int x)
+  {
+    cout<<x<<endl;
+  }
+
+  void func(double y)
+  {
+    cout<<y<<" diff"<<endl;
+  }
+
+  void func(int x, double y)
+  {
+    cout<<(x+y)<<endl;
+  }
+
+};
+int main(int argc, char const *argv[])
+{
+  A obj;
+  cout<<argv[1]<<endl;
+  obj.func(2);
+  obj.func(2.2);
+  obj.func(2, 5); // EX 0 for false
+
+
 }
